976caf8ba9ff35dc0ea302d2674ad7c9c41aed76
Yash-3122 <96653022+Yash-3122@users.noreply.github.com>
2022-04-17 00:11:52 +0530

    Add files via upload

---
+++ Lapindrome (1).py
@@ -0,0 +1,15 @@
+## 20CE112 Yash Patel
+## Github Repository Link
+## https://github.com/Yash-3122/Assignment7
+## Practical 7: LAPINDROME
+
+
+n = int(input())
+for i in range(n):
+    str = input()
+    if str==str[::-1]:
+        print("YES")
+    else:
+        print("NO")
+
+
